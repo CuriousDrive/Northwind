885c782e4163d59d2700e66c04f5ec03cddde6ee
Curious Drive <CuriousDrive@users.noreply.github.com>
2021-03-07 13:58:24 -0500

    Adding another weather type string

--- Northwind_API/Controllers/WeatherForecastController.cs
+++ Northwind_API/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Northwind_API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Very Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
